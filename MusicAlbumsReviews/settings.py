1d7d8da8c450d42b302460b648972ac207769fe0
Sailor4 <ddimitrov8686@gmail.com>
2026-04-21 11:18:43 +0300

    reverted back to debug True

--- MusicAlbumsReviews/settings.py
+++ MusicAlbumsReviews/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6q^i03!py#+30$esks4eb=2&oz24vbwzj)^dnp(30f5yaoe!br'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
